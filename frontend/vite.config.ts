2cb5dbbf03c5e523ea5423c9060a16a7a7555308
laetitia piat <laeptitchat29@gmail.com>
2024-11-17 10:32:23 +0100

    dis vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Assurez-vous que cela correspond à votre Output Directory dans Vercel
+  },
+});
