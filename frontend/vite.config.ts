a7b8559af7e58ad3782053372a3d9785ffa04543
laetitia piat <laeptitchat29@gmail.com>
2024-11-17 16:55:50 +0100

    vite + redirects

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
     outDir: "dist", // Assurez-vous que cela correspond à votre Output Directory dans Vercel
     rollupOptions: {
