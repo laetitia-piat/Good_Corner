a95bc0406529726000a7deefc8c9351cfbeb71bd
laetitia piat <laeptitchat29@gmail.com>
2024-11-17 18:09:56 +0100

    Link

--- frontend/src/components/AdCard.tsx
+++ frontend/src/components/AdCard.tsx
@@ -1,6 +1,8 @@
 // import { Link } from "react-router-dom";
 // import ButtonDelete from "./ButtonDelete";
 
+import { Link } from "react-router-dom";
+
 export type AdCardProps = {
   id: number;
   title: string;
@@ -12,22 +14,24 @@ export type AdCardProps = {
 };
 
 const AdCard = ({ link, pictures, title, price, category }: AdCardProps) => (
-  <div className="ad-card-container">
-    <a className="ad-card-link" href={link}>
-      <img className="ad-card-image" src={pictures} />
-      <div className="ad-card-text">
-        <div className="ad-card-title">{title}</div>
-        <div className="ad-card-price">{price}€</div>
-        <div className="ad-card-category">{category.name}</div>
-      </div>
-      <div className="button-ad-card">
-        {/* <Link to={`update`} className="button button-primary">
+  <Link to={link} className="category-navigation-link">
+    <div className="ad-card-container">
+      <a className="ad-card-link">
+        <img className="ad-card-image" src={pictures} />
+        <div className="ad-card-text">
+          <div className="ad-card-title">{title}</div>
+          <div className="ad-card-price">{price}€</div>
+          <div className="ad-card-category">{category.name}</div>
+        </div>
+        <div className="button-ad-card">
+          {/* <Link to={`update`} className="button button-primary">
           Modifier
         </Link>
         <ButtonDelete /> */}
-      </div>
-    </a>
-  </div>
+        </div>
+      </a>
+    </div>
+  </Link>
 );
 
 export default AdCard;
