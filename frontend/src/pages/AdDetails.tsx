dc22df165ec7a926922c1ba6b34623072bbe1a2f
laetitia piat <laeptitchat29@gmail.com>
2024-11-17 17:33:37 +0100

    test

--- frontend/src/pages/AdDetails.tsx
+++ frontend/src/pages/AdDetails.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import ButtonDelete from "../components/ButtonDelete";
 import { useQuery } from "@apollo/client";
@@ -7,7 +7,7 @@ import { AdByIdDetails } from "../GraphQL/Query";
 const AdDetails = () => {
   const { id }: any = useParams();
   const { loading, error, data } = useQuery(AdByIdDetails, {
-    variables: { getAdByIdId: id },
+    variables: { getAdByIdId: parseInt(id) },
   });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :</p>;
@@ -16,7 +16,7 @@ const AdDetails = () => {
     <>
       <Header />
       <p>TEST</p>
-      <div>
+      {/* <div>
         <h2 className="ad-details-title">{data.getAdById.title}</h2>
         <section className="ad-details">
           <div className="ad-details-image-container">
@@ -59,7 +59,7 @@ const AdDetails = () => {
             </Link>
           </div>
         </section>
-      </div>
+      </div> */}
       <ButtonDelete />
     </>
   );
