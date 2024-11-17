a0671a1ccb04561a3270964d60f449a55460166a
laetitia piat <laeptitchat29@gmail.com>
2024-11-17 15:10:17 +0100

    no test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import RecentAds from "./components/RecentsAds";
 import Layout from "./pages/Layout";
 import AboutPage from "./pages/AboutPage";
-//import AdDetails from "./pages/AdDetails";
+import AdDetails from "./pages/AdDetails";
 import AdbyCategory from "./pages/AdByCategory";
 import UpdateAdForm from "./pages/UpdateAdForm";
 import NewCategoryFormPage from "./pages/NewCategoryForm";
@@ -21,7 +21,7 @@ const App = () => {
           <Route path="about" element={<AboutPage />} />
           <Route path="ad/new" element={<TestNewAdForm />} />
           <Route path="ad/search/:keyword" element={<AdSearchPage />} />
-          <Route path="ad/:id" element={<AboutPage />} />
+          <Route path="ad/:id" element={<AdDetails />} />
           <Route path="ad/:id/update" element={<UpdateAdForm />} />
           <Route path="category/new" element={<NewCategoryFormPage />} />
           <Route path="category/:name" element={<AdbyCategory />} />
