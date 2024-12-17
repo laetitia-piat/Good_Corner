fdc382cd7860b3a1129eac907e7ffdb502fc2ca0
laetitia piat <laeptitchat29@gmail.com>
2024-12-17 15:22:08 +0100

    docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist", // Assurez-vous que cela correspond à votre Output Directory dans Vercel
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
+  server: { host: true },
 });
