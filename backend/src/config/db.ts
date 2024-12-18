b0437ee838c36cfca88c90bccc9c738d9936498c
laetitia piat <laeptitchat29@gmail.com>
2024-12-18 09:57:57 +0100

    docker postgres

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,8 +5,11 @@ import { Tag } from "../entities/Tag";
 import { Picture } from "../entities/Picture";
 
 export const dataSourceGoodCorner = new DataSource({
-  database: "good_corner.sqlite",
-  type: "sqlite",
+  type: "postgres",
+  host: "db",
+  username: "postgres",
+  database: "postgres",
+  password: "example",
   entities: [Ad, Category, Tag, Picture],
   synchronize: true,
   logging: ["error", "query"],
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,26 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { dataSourceGoodCorner } from "./config/db";
-import AdResolver from "./resolvers/AdResolver";
 import { buildSchema } from "type-graphql";
+import AdResolver from "./resolvers/AdResolver";
 import CategoryResolver from "./resolvers/CategoryResolver";
 import TagResolver from "./resolvers/TagResolver";
 
 const start = async () => {
   await dataSourceGoodCorner.initialize();
+
   const schema = await buildSchema({
     resolvers: [AdResolver, CategoryResolver, TagResolver],
-    emitSchemaFile: true,
   });
 
   const server = new ApolloServer({
     schema,
-    // cors: {origin : 'http://localhost:3000'}
   });
-
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
+
   console.log(`🚀 Server listening at: ${url}`);
+  console.log("test hot reload");
 };
 start();
